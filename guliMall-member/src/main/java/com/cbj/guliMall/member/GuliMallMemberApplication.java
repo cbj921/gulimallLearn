5132a02f9aa9135f82f63e1702d9db5e5cca68db
chenbangjun <447933991@qq.com>
2023-06-16 22:58:08 +0800

    完成nacos注册、配置，gateway项目创建

--- guliMall-member/src/main/java/com/cbj/guliMall/member/GuliMallMemberApplication.java
+++ guliMall-member/src/main/java/com/cbj/guliMall/member/GuliMallMemberApplication.java
@@ -2,8 +2,12 @@ package com.cbj.guliMall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.cbj.guliMall.member.feign")
 public class GuliMallMemberApplication {
 
 	public static void main(String[] args) {
